b258af662da5c8e1e5da7281a1b216781705f586
Andy Gurden <as.gurden@gmail.com>
2014-08-12 23:29:36 +0100

    Using jasmine.Clock.useMock() instead of async tests

--- jasmine-test/spec/override-diff-behavior.js
+++ jasmine-test/spec/override-diff-behavior.js
@@ -28,136 +28,122 @@ describe('override-diff-behaviour', function() {
   });
 
   it('should use original diffing behavior (Diff, LCS, DiffParser) by default', function() {
-    runs(function() {
-      var mglyElem = createMergely('someid', testingOptions('left text', 'right text'));
-    });
-    waits(0);
-    runs(function() {
-      expect(Mgly.diff).toHaveBeenCalled();
-      expect(Mgly.DiffParser).toHaveBeenCalled();
-      expect(Mgly.LCS).toHaveBeenCalled();
-    });
+    jasmine.Clock.useMock();
+
+    var mglyElem = createMergely('someid', testingOptions('left text', 'right text'));
+    jasmine.Clock.tick(0);
+
+    expect(Mgly.diff).toHaveBeenCalled();
+    expect(Mgly.DiffParser).toHaveBeenCalled();
+    expect(Mgly.LCS).toHaveBeenCalled();
   });
 
   it('should use the DiffLines function from diff_behavior option when asked to provide a diff', function() {
-    var diffLinesSpy;
-    var mglyElem;
-    runs(function() {
-      diffLinesSpy = jasmine.createSpy('DiffLines').andReturn('normal form');
-      mglyElem = createMergely('someid',
-        testingOptions('left text', 'right text', { diff_behavior: $.extend(spiedDiffBehaviors, { DiffLines: diffLinesSpy }) })
-      );
-    });
-    waits(0);
-    runs(function() {
-      Mgly.diff.reset();
-      diffLinesSpy.reset();
-      expect(mglyElem.mergely('diff')).toBe('normal form');
-      expect(Mgly.diff).not.toHaveBeenCalled();
-      expect(diffLinesSpy).toHaveBeenCalled();
-    });
+    jasmine.Clock.useMock();
+
+    var diffLinesSpy = jasmine.createSpy('DiffLines').andReturn('normal form');
+    var mglyElem = createMergely('someid',
+      testingOptions('left text', 'right text', { diff_behavior: $.extend(spiedDiffBehaviors, { DiffLines: diffLinesSpy }) })
+    );
+    jasmine.Clock.tick(0);
+
+    Mgly.diff.reset();
+    diffLinesSpy.reset();
+    expect(mglyElem.mergely('diff')).toBe('normal form');
+    expect(Mgly.diff).not.toHaveBeenCalled();
+    expect(diffLinesSpy).toHaveBeenCalled();
   });
 
   it('should use the DiffLines and Parser functions from diff_behavior option when the text is first diffed', function() {
-    var diffLinesSpy, parseSpy;
-    var mglyElem;
-    runs(function() {
-      diffLinesSpy = jasmine.createSpy('DiffLines').andReturn('normal form');
-      parseSpy = jasmine.createSpy('Parse').andReturn([{
-        'lhs-line-from': 1,
-        'lhs-line-to': 1,
-        'op': "c",
-        'rhs-line-from': 1,
-        'rhs-line-to': 1
-      }]);
-      mglyElem = createMergely('someid',
-        testingOptions('left text\nline 2', 'right text\nline 2', {
-          diff_behavior: $.extend(spiedDiffBehaviors, { DiffLines: diffLinesSpy, Parse: parseSpy })
-        })
-      );
-    });
-    waits(0);
-    runs(function() {
-      expect(Mgly.diff).not.toHaveBeenCalled();
-      expect(Mgly.DiffParser).not.toHaveBeenCalled();
-      expect(diffLinesSpy).toHaveBeenCalled();
-      expect(parseSpy).toHaveBeenCalled();
+    jasmine.Clock.useMock();
 
-      var lines = mglyElem.find('#someid-editor-lhs .CodeMirror-lines');
-      var lineElems = lines.find('div > div:not([style]):not([class]) > div');
-      expect($(lineElems[0]).has('.CodeMirror-linebackground').length).toBe(0);
-      expect($(lineElems[1]).has('.CodeMirror-linebackground').length).toBeGreaterThan(0);
-    });
+    var diffLinesSpy = jasmine.createSpy('DiffLines').andReturn('normal form'),
+        parseSpy = jasmine.createSpy('Parse').andReturn([{
+          'lhs-line-from': 1,
+          'lhs-line-to': 1,
+          'op': "c",
+          'rhs-line-from': 1,
+          'rhs-line-to': 1
+        }]);
+    var mglyElem = createMergely('someid',
+      testingOptions('left text\nline 2', 'right text\nline 2', {
+        diff_behavior: $.extend(spiedDiffBehaviors, { DiffLines: diffLinesSpy, Parse: parseSpy })
+      })
+    );
+    jasmine.Clock.tick(0);
+
+    expect(Mgly.diff).not.toHaveBeenCalled();
+    expect(Mgly.DiffParser).not.toHaveBeenCalled();
+    expect(diffLinesSpy).toHaveBeenCalled();
+    expect(parseSpy).toHaveBeenCalled();
+
+    var lines = mglyElem.find('#someid-editor-lhs .CodeMirror-lines');
+    var lineElems = lines.find('div > div:not([style]):not([class]) > div');
+    expect($(lineElems[0]).has('.CodeMirror-linebackground').length).toBe(0);
+    expect($(lineElems[1]).has('.CodeMirror-linebackground').length).toBeGreaterThan(0);
   });
 
   it('should use the DiffChars function from diff_behavior option to diff each line if lcs option is on', function() {
-    var diffCharsSpy;
-    var mglyElem;
-    runs(function() {
-      diffCharsSpy = jasmine.createSpy('DiffChars').andCallFake(function(l, r, added, removed) {
-        added(0, 2);
-        removed(4, 7);
-      });
-      mglyElem = createMergely('someid',
-        testingOptions('some text', 'other text', {
-          diff_behavior: $.extend(spiedDiffBehaviors, { DiffChars: diffCharsSpy })
-        })
-      );
-    });
-    waits(0);
-    runs(function() {
-      expect(Mgly.LCS).not.toHaveBeenCalled();
-      expect(diffCharsSpy).toHaveBeenCalled();
+    jasmine.Clock.useMock();
 
-      expect(mglyElem.find('.CodeMirror-lines > div > div:not(.CodeMirror-measure) pre .rhs.mergely.ch.a').text()).toBe('ot');
-      expect(mglyElem.find('.CodeMirror-lines > div > div:not(.CodeMirror-measure) pre .lhs.mergely.ch.d').text()).toBe(' te');
+    var diffCharsSpy = jasmine.createSpy('DiffChars').andCallFake(function(l, r, added, removed) {
+      added(0, 2);
+      removed(4, 7);
     });
+    var mglyElem = createMergely('someid',
+      testingOptions('some text', 'other text', {
+        diff_behavior: $.extend(spiedDiffBehaviors, { DiffChars: diffCharsSpy })
+      })
+    );
+    jasmine.Clock.tick(0);
+
+    expect(Mgly.LCS).not.toHaveBeenCalled();
+    expect(diffCharsSpy).toHaveBeenCalled();
+
+    expect(mglyElem.find('.CodeMirror-lines > div > div:not(.CodeMirror-measure) pre .rhs.mergely.ch.a').text()).toBe('ot');
+    expect(mglyElem.find('.CodeMirror-lines > div > div:not(.CodeMirror-measure) pre .lhs.mergely.ch.d').text()).toBe(' te');
   });
 
   it('should pass mergely settings as the last parameter to each of DiffLines, DiffChars, and Parse', function() {
+    jasmine.Clock.useMock();
+
     var lineSettings, parseSettings, charSettings;
-    var mglyElem;
-    runs(function() {
-      mglyElem = createMergely('someid',
-        testingOptions('some text', 'other text', {
-          diff_behavior: $.extend({}, spiedDiffBehaviors, {
-            DiffLines: function() { lineSettings = arguments[arguments.length-1]; return spiedDiffBehaviors.DiffLines.apply(spiedDiffBehaviors, arguments); },
-            Parse: function() { parseSettings = arguments[arguments.length-1]; return spiedDiffBehaviors.Parse.apply(spiedDiffBehaviors, arguments); },
-            DiffChars: function() { charSettings = arguments[arguments.length-1]; return spiedDiffBehaviors.DiffChars.apply(spiedDiffBehaviors, arguments); }
-          }),
-          testOption: 'success'
-        })
-      );
-    });
-    waits(0);
-    runs(function() {
-      expect(lineSettings).toBe(parseSettings);
-      expect(parseSettings).toBe(charSettings);
-      expect(charSettings.testOption).toBe('success');
-    });
+    var mglyElem = createMergely('someid',
+      testingOptions('some text', 'other text', {
+        diff_behavior: $.extend({}, spiedDiffBehaviors, {
+          DiffLines: function() { lineSettings = arguments[arguments.length-1]; return spiedDiffBehaviors.DiffLines.apply(spiedDiffBehaviors, arguments); },
+          Parse: function() { parseSettings = arguments[arguments.length-1]; return spiedDiffBehaviors.Parse.apply(spiedDiffBehaviors, arguments); },
+          DiffChars: function() { charSettings = arguments[arguments.length-1]; return spiedDiffBehaviors.DiffChars.apply(spiedDiffBehaviors, arguments); }
+        }),
+        testOption: 'success'
+      })
+    );
+    jasmine.Clock.tick(0);
+
+    expect(lineSettings).toBe(parseSettings);
+    expect(parseSettings).toBe(charSettings);
+    expect(charSettings.testOption).toBe('success');
   });
 
   it('should pass use the diff behavior object for the context of DiffLines, DiffChars, and Parse', function() {
+    jasmine.Clock.useMock();
+
     var lineThis, parseThis, charThis;
-    var mglyElem;
     var diff_behavior = $.extend({}, spiedDiffBehaviors, {
       DiffLines: function() { lineThis = this; return spiedDiffBehaviors.DiffLines.apply(spiedDiffBehaviors, arguments); },
       Parse: function() { parseThis = this; return spiedDiffBehaviors.Parse.apply(spiedDiffBehaviors, arguments); },
       DiffChars: function() { charThis = this; return spiedDiffBehaviors.DiffChars.apply(spiedDiffBehaviors, arguments); }
     });
-    runs(function() {
-      mglyElem = createMergely('someid',
-        testingOptions('some text', 'other text', {
-          diff_behavior: diff_behavior,
-          testOption: 'success'
-        })
-      );
-    });
-    waits(0);
-    runs(function() {
-      expect(lineThis).toBe(parseThis);
-      expect(parseThis).toBe(charThis);
-      expect(charThis).toBe(diff_behavior);
-    });
+    var mglyElem = createMergely('someid',
+      testingOptions('some text', 'other text', {
+        diff_behavior: diff_behavior,
+        testOption: 'success'
+      })
+    );
+    jasmine.Clock.tick(0);
+
+    expect(lineThis).toBe(parseThis);
+    expect(parseThis).toBe(charThis);
+    expect(charThis).toBe(diff_behavior);
   });
 });
