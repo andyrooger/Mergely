024b4e064f2c3f69e32de9993641c0b032aee078
Andy Gurden <as.gurden@gmail.com>
2014-08-13 21:32:48 +0100

    Made color fetching work for other colors too

--- jasmine-test/spec/colors-from-css.js
+++ jasmine-test/spec/colors-from-css.js
@@ -1,16 +1,18 @@
 describe('colors-from-css', function() {
-  var knownColors = {
+  var changeTypeKnownColors = {
     a: 'rgb(111, 0, 0)',
     c: 'rgb(0, 111, 0)',
     d: 'rgb(0, 0, 111)'
   };
+  var testColor = 'rgb(42, 42, 42)';
 
   var styleElem;
   beforeEach(function() {
     var style = '';
-    for(var cls in knownColors) {
-      style += '.mergely.lhs.start.' + cls + ' { border-top-color: ' + knownColors[cls] + ' }'
+    for(var cls in changeTypeKnownColors) {
+      style += '.mergely.lhs.start.' + cls + ' { border-top-color: ' + changeTypeKnownColors[cls] + ' }'
     }
+    style += '.some.test.color { background-color: ' + testColor + ' }';
     styleElem = $('<style>').text(style);
     getSandbox().append(styleElem);
   });
@@ -19,17 +21,23 @@ describe('colors-from-css', function() {
     styleElem = null;
   });
 
-  it('should take foreground color from diff top border color if fgcolor is \'auto\'', function () {
-    var mglyElem = createMergely('someid', testingOptions('left text', 'right text', { fgcolor: 'auto' }));
-    expect(mglyElem.mergely('options').fgcolor).toEqual(knownColors);
+  ['a', 'c', 'd'].forEach(function (changetype) {
+    it('should take changetype ' + changetype + ' foreground color from the class and property specified if it\'s of the form auto:prop:classes', function () {
+      var passColors = { a: '#000', c: '#000', d: '#000' };
+      passColors[changetype] = 'auto:backgroundColor:some test color';
+      var mglyElem = createMergely('someid', testingOptions('left text', 'right text', { fgcolor: passColors }));
+      for(var ct in mglyElem.mergely('options').fgcolor) {
+        expect(mglyElem.mergely('options').fgcolor[ct]).toBe(ct === changetype ? testColor : '#000');
+      }
+    });
   });
 
-  ['a', 'c', 'd'].forEach(function (changetype) {
-    it('should take changetype ' + changetype + ' foreground color from ' + changetype + ' diff border colors if fgcolor.' + changetype + ' is \'auto\'', function () {
-      var passColors = { a: '#000', c: '#000', d: '#000' };
-      passColors[changetype] = 'auto';
-      var mglyElem = createMergely('someid', testingOptions('left text', 'right text', { fgcolor: passColors }));
-      expect(mglyElem.mergely('options').fgcolor[changetype]).toEqual(knownColors[changetype]);
+  ['bgcolor', 'vpcolor', 'hcolor'].forEach(function (colorProp) {
+    it('should take color ' + colorProp + ' from the class and property specified if it\'s of the form auto:prop:classes', function () {
+      var passColors = {};
+      passColors[colorProp] = 'auto:backgroundColor:some test color';
+      var mglyElem = createMergely('someid', testingOptions('left text', 'right text', passColors));
+      expect(mglyElem.mergely('options')[colorProp]).toBe(testColor );
     });
   });
 
